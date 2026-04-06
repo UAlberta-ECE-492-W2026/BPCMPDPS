2b3646f8ef7f69e284ee4ca223c5b4be9e517d0f
Kattily-eng <113124949+Kattily-eng@users.noreply.github.com>
2026-04-06 00:25:32 -0600

    Delete Model directory

--- Model/MSECalx.py
+++
@@ -1,6 +0,0 @@
-
-from sklearn.metrics import mean_squared_error
-
-#to create the MSE calc for the gradient descent
-def MSECalc(price_true, price_pred):
-    return mean_squared_error(price_true, price_pred)
